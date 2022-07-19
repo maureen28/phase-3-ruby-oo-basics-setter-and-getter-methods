1866975abfb91b2750b4faef0323898bb9ea764a
maureen <maureen.murimi@moringaschool.com>
2022-07-19 19:08:02 +0300

    commit tree 2f02904eebfa2b55b6d6c652d136fdb8a42e6d14

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,3 @@
 class Dog
     attr_accessor :name, :breed
-    def dog(name,breed)
-        @name=name
-        @breed=breed
-    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,3 @@
 class Person
    attr_accessor :name, :job
-   def person(name,job)
-    @name=name
-    @job=job
-   end
 end
